60b1d94c16c15a02606919d0366cc820f75a7d8d
CercelaruCatalin <cercelaru.catalin@gmail.com>
2026-06-08 16:24:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,6 @@ def on_year(station, year):
     return jsonify(result)
 
 if __name__ == "__main__":
-    # CRUCIAL CHANGE FOR CLOUD RUN:
     # Get the port from the environment variable
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
